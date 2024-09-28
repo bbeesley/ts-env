f1383360be507d4c27f5f7e5f3db791ee8214dbf
Bill Beesley <bbeesley@me.com>
2024-09-28 10:52:50 +0100

    chore: fix up gitignore

--- .tsimp/compiled/src_$$_boolean-or-throw_ts.js
+++
@@ -1,6 +0,0 @@
-import boolean from './boolean.js';
-import orThrow from './or-throw.js';
-export default function booleanOrThrow(key) {
-    return orThrow(boolean, 'boolean')(key);
-}
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYm9vbGVhbi1vci10aHJvdy5qcyIsInNvdXJjZVJvb3QiOiIvVm9sdW1lcy9Qcm9qZWN0cy90cy1lbnYvIiwic291cmNlcyI6WyJzcmMvYm9vbGVhbi1vci10aHJvdy50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQSxPQUFPLE9BQU8sTUFBTSxjQUFjLENBQUM7QUFDbkMsT0FBTyxPQUFPLE1BQU0sZUFBZSxDQUFDO0FBRXBDLE1BQU0sQ0FBQyxPQUFPLFVBQVUsY0FBYyxDQUFDLEdBQVc7SUFDaEQsT0FBTyxPQUFPLENBQUMsT0FBTyxFQUFFLFNBQVMsQ0FBQyxDQUFDLEdBQUcsQ0FBQyxDQUFDO0FBQzFDLENBQUMifQ==
--- .tsimp/compiled/src_$$_string-array_ts.js
+++
@@ -1,8 +0,0 @@
-export default function stringArray(key, separator = ',') {
-    const rawValue = process.env[key];
-    if (!rawValue) {
-        return undefined;
-    }
-    return rawValue.split(separator);
-}
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoic3RyaW5nLWFycmF5LmpzIiwic291cmNlUm9vdCI6Ii9Wb2x1bWVzL1Byb2plY3RzL3RzLWVudi8iLCJzb3VyY2VzIjpbInNyYy9zdHJpbmctYXJyYXkudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUEsTUFBTSxDQUFDLE9BQU8sVUFBVSxXQUFXLENBQUMsR0FBVyxFQUFFLFNBQVMsR0FBRyxHQUFHO0lBQzlELE1BQU0sUUFBUSxHQUFHLE9BQU8sQ0FBQyxHQUFHLENBQUMsR0FBRyxDQUFDLENBQUM7SUFFbEMsSUFBSSxDQUFDLFFBQVEsRUFBRSxDQUFDO1FBQ2QsT0FBTyxTQUFTLENBQUM7SUFDbkIsQ0FBQztJQUVELE9BQU8sUUFBUSxDQUFDLEtBQUssQ0FBQyxTQUFTLENBQUMsQ0FBQztBQUNuQyxDQUFDIn0=
